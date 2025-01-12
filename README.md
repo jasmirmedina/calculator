128458606ad54a08e564ce90207a188bf490b321
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-12 00:43:49 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculator
+Simple calculator made following the course of [The Odin Project](https://www.theodinproject.com).
+
+## Live:
+![demo](assets/images/demo.png)
+
+### Resources:
+- [minireset.css](https://jgthms.com/minireset.css/)
+- [Orbitron Font](https://fonts.google.com/specimen/Orbitron)
---
+++ src/main.js
@@ -0,0 +1,59 @@
+const btns = document.querySelector(".btns");
+const numberInput = document.querySelector(".number-input");
+
+const operationSigns = ["/", "x", "-", "+", "="];
+
+const values = {
+  first: 0,
+  second:0
+};
+
+let operator = "";
+
+function doOperation(n1, n2, op) {
+  const operations = {
+    "/": (n1, n2) => n1 / n2,
+    "x": (n1, n2) => n1 * n2,
+    "-": (n1, n2) => n1 - n2,
+    "+": (n1, n2) => n1 + n2
+  };
+
+  const un1 = parseInt(n1);
+  const un2 = parseInt(n2);
+
+  return operations[op](un1, un2);
+}
+
+btns.addEventListener("click", (e) => {
+  const tagName = e.target.tagName;
+  const contentText = e.target.textContent;
+
+  if(tagName === "BUTTON" && !operationSigns.includes(contentText)) {
+    numberInput.value += contentText;
+  }
+
+  if(
+    tagName === "BUTTON" && operationSigns.includes(contentText) &&
+    contentText !== "=" && numberInput.value
+  ) {
+    values.first = parseFloat(numberInput.value);
+
+    numberInput.value = "";
+
+    operator = contentText;
+  }
+
+  if(operator && numberInput.value) {
+    values.second = parseFloat(numberInput.value);
+  }
+
+  if(operator && contentText === "=") {
+    numberInput.value = doOperation(values.first, values.second, operator);
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if(e.key === "Backspace") {
+    numberInput.value = numberInput.value.split("").slice(0, -1).join("");
+  }
+});
